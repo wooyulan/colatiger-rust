032781d63b6459b3873e20945aa140c49d0bd4e4
wooyulan <wooyulan@163.com>
2023-11-22 16:11:23 +0800

    update

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,11 +2,13 @@ use milvus_db::init_milvus_client;
 use snowflake::init_snowflak;
 use s3::init_s3;
 use postgres::init_postgres;
+use redis::init_redis;
 
 pub mod milvus_db;
 pub mod snowflake;
 pub mod s3;
 pub mod postgres;
+pub mod redis;
 
 
 pub async fn init_db() {
@@ -21,5 +23,8 @@ pub async fn init_db() {
 
     // 连接pgsql
     init_postgres().await;
+
+    // 连接redis
+    init_redis().await;
 
 }
